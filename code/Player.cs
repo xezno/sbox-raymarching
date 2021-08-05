b82e006aad3c49fc38910fb1c74d52022f045007
Alex <alex@gu3.me>
2021-08-05 15:24:24 +0100

    Terrain
    Update to latest shader stuff
    Custom speedy noclip controller

--- code/Player.cs
+++ code/Player.cs
@@ -8,7 +8,7 @@ namespace GodRealm
 		{
 			SetModel( "models/citizen/citizen.vmdl" ); // This doesn't really matter cos we don't see it
 			
-			Controller = new NoclipController();
+			Controller = new NoClipController();
 			Animator = new StandardPlayerAnimator();
 			Camera = new FirstPersonCamera();
 
